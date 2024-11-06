06eefce9ab5b5b8ff473a88a90953b38906ea89f
deokkeun <84825191+deokkeun@users.noreply.github.com>
2024-11-07 02:07:07 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        if(0 <= n && n <= 1000000) {
+            while(n > 0) {
+                answer += n % 10;
+                n /= 10;
+            }
+        }
+        return answer;
+    }
+}
