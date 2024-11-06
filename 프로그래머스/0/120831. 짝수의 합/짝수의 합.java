fd0dda4ec5bd2260bdbcbc95dc8b6f4447a01398
deokkeun <84825191+deokkeun@users.noreply.github.com>
2024-11-07 00:22:28 +0900

    [level 0] Title: 짝수의 합, Time: 2.30 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -1,8 +1,10 @@
+import java.util.stream.IntStream;
 class Solution {
     public int solution(int n) {
         if(0 < n && n <= 1000) {
-            int a = (n % 2 == 0) ? n : n - 1; 
-            return a / 2 * (2 + a) / 2;
+            return IntStream.rangeClosed(0, n)
+                    .filter(v -> v % 2 == 0)
+                    .sum();
         }
         return -1;
     }
