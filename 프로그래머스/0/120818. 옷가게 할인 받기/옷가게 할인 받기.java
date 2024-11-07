3ba95df807feeaaa39cfd156040bb8a67e6f7c99
deokkeun <84825191+deokkeun@users.noreply.github.com>
2024-11-08 07:47:37 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int price) {
+        return price >= 500000 ? price * 80 / 100 : 
+               price >= 300000 ? price * 90 / 100 : 
+               price >= 100000 ? price * 95 / 100 : 
+               price;
+    }
+}
