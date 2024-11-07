191b495ce21b51390111d4293919ccdafdfa5770
deokkeun <84825191+deokkeun@users.noreply.github.com>
2024-11-07 15:51:49 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.05 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,5 +1,8 @@
 class Solution {
     public int solution(String str1, String str2) {
-        return str1.contains(str2) ? 1 : 2;
+        if(1 <= str1.length() && str1.length() <= 100 && 1 <= str2.length() && str2.length() <= 100) {
+            return str1.contains(str2) ? 1 : 2;   
+        }
+        return -1;
     }
 }
