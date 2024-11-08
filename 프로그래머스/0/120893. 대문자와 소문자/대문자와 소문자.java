8e1bfe8b486dee3cbf52c2756f5c75e7fa483257
deokkeun <84825191+deokkeun@users.noreply.github.com>
2024-11-08 19:30:33 +0900

    [level 0] Title: 대문자와 소문자, Time: 4.55 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -1,13 +1,7 @@
+import java.util.stream.Collectors;
+
 class Solution {
-    public String solution(String my_string) {
-        String str = "";
-        for(char c : my_string.toCharArray()) {
-            if(65 <= c && c <= 90) {
-                str += Character.toLowerCase(c);
-            } else {
-                str += Character.toUpperCase(c);
-            }
-        }
-        return str;
+    public String solution(String myString) {
+        return myString.chars().mapToObj(operand -> String.valueOf((char) (Character.isLowerCase(operand) ? Character.toUpperCase(operand) : Character.toLowerCase(operand)))).collect(Collectors.joining());
     }
 }
