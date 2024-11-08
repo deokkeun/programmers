64974b6d6169d2e2d4f79cdde0d043c1ceedf4a3
deokkeun <84825191+deokkeun@users.noreply.github.com>
2024-11-08 15:38:40 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -1,8 +1,5 @@
 class Solution {
     public int solution(int hp) {
-        int a = hp / 5;
-        int b = (hp % 5) / 3;
-        int c = (hp % 5) % 3;
-        return a + b + c;
+        return hp / 5 + (hp % 5 / 3) + (hp % 5 % 3);
     }
 }
