02f30d333ff3e7fba47d0dd6aa58643633508528
deokkeun <84825191+deokkeun@users.noreply.github.com>
2024-11-14 01:04:42 +0900

    [level 0] Title: 외계행성의 나이, Time: 12.73 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,9 +1,8 @@
 class Solution {
     public String solution(int age) {
         String answer = "";
-        while(age > 0) {
-            answer = (char)(age % 10 + 97) + answer;
-            age /= 10;
+        for(char c : String.valueOf(age).toCharArray()) {
+            answer += (char)('a' + (c - '0'));
         }
         return answer;
     }
