627a672e1f2bfb52c7312307ab19844d4ec4df65
deokkeun <84825191+deokkeun@users.noreply.github.com>
2024-11-21 00:12:30 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.05 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,14 +1,13 @@
 import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = Arrays.copyOf(num_list, num_list.length + 1);
-        int last = num_list.length - 1;
         
-        if(num_list[last] > num_list[last - 1]) {
-            answer[answer.length - 1] = num_list[last] - num_list[last - 1];
-        } else {
-            answer[answer.length - 1] = num_list[last] * 2;
-        }
+        int last = num_list[num_list.length - 1];
+        int before = num_list[num_list.length - 2];
+        
+        answer[answer.length - 1] = last > before ? last - before : last * 2;
         
         return answer;
     }
