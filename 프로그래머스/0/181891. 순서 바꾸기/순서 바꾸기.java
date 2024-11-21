7fd59e030a4fd2f3d752ad556fcd62402d8ee8cf
deokkeun <84825191+deokkeun@users.noreply.github.com>
2024-11-21 10:31:07 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.03 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,20 +1,15 @@
 import java.util.Arrays;
-import java.util.List;
-import java.util.ArrayList;
 
 class Solution {
-    public List<Integer> solution(int[] num_list, int n) {
-        List<Integer> list = new ArrayList<>();
-        int[] before = Arrays.copyOfRange(num_list, n, num_list.length);
-        int[] last = Arrays.copyOfRange(num_list, 0, n);
-        
-        for(int i : before) {
-            list.add(i);
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[num_list.length];
+        int index = 0;
+        for(int i = n; i < num_list.length; i++) {
+            answer[index++] = num_list[i];
         }
-        for(int i : last) {
-            list.add(i);
+        for(int i = 0; i < n; i++) {
+            answer[index++] = num_list[i];
         }
-        
-        return list;
+        return answer;
     }
 }
