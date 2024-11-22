b4e791817b7875d1d3b928fe959e8c19496c3cf7
deokkeun <84825191+deokkeun@users.noreply.github.com>
2024-11-22 11:55:20 +0900

    [level 0] Title: 약수 구하기, Time: 0.16 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public List<Integer> solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        for(int i = 1; i <= n; i++) {
+            if(n % i == 0) {
+                list.add(i);
+            }
+        }
+        return list;
+    }
+}
