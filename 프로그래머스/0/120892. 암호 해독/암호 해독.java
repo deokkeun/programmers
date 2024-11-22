983f3fad20486fe3b0dde7a91a3e4b5f8ea6d94d
deokkeun <84825191+deokkeun@users.noreply.github.com>
2024-11-22 13:55:23 +0900

    [level 0] Title: 암호 해독, Time: 2.76 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -1,9 +1,11 @@
 class Solution {
     public String solution(String cipher, int code) {
         String answer = "";
-        for(int i = code - 1; i < cipher.length(); i += code) {
-            answer += cipher.charAt(i);
+
+        for (int i = code; i <= cipher.length(); i += code) {
+            answer += cipher.substring(i - 1, i);
         }
+
         return answer;
     }
 }
