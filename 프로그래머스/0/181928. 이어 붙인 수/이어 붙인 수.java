ed4b6179ec7bf153c4b86becca54767021ef1ff3
deokkeun <84825191+deokkeun@users.noreply.github.com>
2024-11-22 13:47:35 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.03 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -1,16 +1,16 @@
 class Solution {
     public int solution(int[] num_list) {
-        int odd = 0;
         int even = 0;
+        int odd = 0;
         for(int i : num_list) {
-            if(i % 2 == 1) {
-                odd *= 10;
-                odd += i;
-            } else {
+            if(i % 2 == 0) {
                 even *= 10;
                 even += i;
+            } else {
+                odd *= 10;
+                odd += i;
             }
         }
-        return odd + even;
+        return even + odd;
     }
 }
